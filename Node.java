9ed1d04c00575d42e348e6e2623d32c5dac4f525
cmagnus <cmagnus@gmail.com>
2017-03-02 14:06:40 -0500

    Adds properties to Node class

--- Node.java
+++ Node.java
@@ -1,3 +1,12 @@
 public class Node{
-
+  // Properties
+  private int data;
+  private Node next;
+/*
+  
+  + getData(): int
++ setData(int) : void
++ getNext() : Node
++ setNext(Node) : void
++ print() : void */
 }

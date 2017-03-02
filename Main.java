b00508d34dffd8696647299f5137c305a288c167
cmagnus <cmagnus@gmail.com>
2017-03-02 14:13:33 -0500

    Adds getters/setters to Node

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main{
-  
-  
-  
   public static void main(String [ ] args) {
+    // test default constructor
+    /*Node myNode = new Node();
+    System.out.println(myNode.getData());
+    System.out.println(myNode.getNext());*/
+    Node myOtherNode = new Node(5);
+    System.out.println(myOtherNode.getData());
+    System.out.println(myOtherNode.getNext());
   }
 }
--- Node.java
+++ Node.java
@@ -14,11 +14,25 @@ public class Node{
     this.next = null;
   }
   
-/*
+  // Getter/Setter Methods
   
-  + getData(): int
-+ setData(int) : void
-+ getNext() : Node
-+ setNext(Node) : void
+  public int getData() {
+    return this.data;
+  }
+  
+  public void setData(int data) {
+    this.data = data;
+  }
+  
+  public Node getNext() {
+    return this.next;
+  }
+  
+  public void setNext(Node next) {
+    this.next = next;
+  }
+  
+  
+/*
 + print() : void */
 }

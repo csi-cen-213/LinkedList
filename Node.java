730042f43cbb7bf8c8ac60d55c09ea0e59a08e14
cmagnus <cmagnus@gmail.com>
2017-03-02 14:09:18 -0500

    Adds constructors to Node

--- Node.java
+++ Node.java
@@ -2,6 +2,18 @@ public class Node{
   // Properties
   private int data;
   private Node next;
+  
+  // Constructor
+  public Node() {
+    this.data = 0;
+    this.next = null;
+  }
+  
+  public Node(int data) {
+    this.data = data;
+    this.next = null;
+  }
+  
 /*
   
   + getData(): int

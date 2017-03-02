e456e4dc23d8b2991f4746ee03b374e3ab2d0bb3
cmagnus <cmagnus@gmail.com>
2017-03-02 14:24:43 -0500

    Adds properties to SinglyLinkedList class

--- Main.java
+++ Main.java
@@ -4,10 +4,13 @@ public class Main{
     /*Node myNode = new Node();
     System.out.println(myNode.getData());
     System.out.println(myNode.getNext());*/
-    Node myOtherNode = new Node(5);
+    /*Node myOtherNode = new Node(5);
     System.out.println(myOtherNode.getData());
     System.out.println(myOtherNode.getNext());
     // test print
-    myOtherNode.print();
+    myOtherNode.print();*/
+    
+    SinglyLinkedList ll = new SinglyLinkedList();
+    System.out.println(ll);
   }
 }
--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,3 +1,16 @@
 public class SinglyLinkedList{
+  // Properties
+  private Node head = null;
+  private int size = 0;
 
+  
+
+/*+ getHead(): Node 
++ setHead(Node): void
++ getSize() : int
++ isEmpty():Boolean
++ find(int): Node
++ add(int, Node) : void
++ add(int, int) : void
++ print() : void*/
 }
